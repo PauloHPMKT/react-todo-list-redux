eb137b4765e43c4003fdbaeb52ce9897efb96a14
Paulo Sergio <pauloserg861@gmail.com>
2022-02-17 18:24:47 -0300

    criando estrutura de Aplicação

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Input from './components/Input'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Lista de tarefas</h1>
+      <Input />
+    </div>
+  );
+}
+
+export default App
